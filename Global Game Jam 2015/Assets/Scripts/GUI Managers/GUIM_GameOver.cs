1174df26fe0fe03e7479b2dccca9321c4b9c6d3c
Peter Keen <peterfkeen@hotmail.com>
2015-01-25 10:39:54 +0000

    Game over menu buttons selectable by keyboard
    w - to scroll up
    s - scroll down
    space - selection

--- Global Game Jam 2015/Assets/Scripts/GUI Managers/GUIM_GameOver.cs
+++ Global Game Jam 2015/Assets/Scripts/GUI Managers/GUIM_GameOver.cs
@@ -1,33 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GUIM_GameOver : MonoBehaviour
 {
+	public List<string> menu = new List<string> ();
+
+	public bool[] buttons;
+
 	int globalScore = 0;
+	private int selectedIndex = 0;
 
 	// Use this for initialization
 	void Start ()
 	{
 		globalScore = PlayerPrefs.GetInt ("Global Score");
+		menu.Add("Restart");
+		menu.Add("Main Menu");
+		menu.Add("Quit");
+		buttons = new bool[menu.Count];
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+
 	}
+	
 
 	void OnGUI()
 	{
 		GUI.Label (new Rect (Screen.width / 2 - 50, 10, 100, 30), "Global Score: " + globalScore);
 
-		if (GUI.Button(new Rect(Screen.width / 2 - 125, 100, 250, 30), "Restart"))
+		for (int count = 0; count < menu.Count; count++) {
+			GUI.SetNextControlName(menu[count]);
+			buttons[count] = GUI.Button(new Rect(Screen.width / 2 - 125, 100 * (count + 1), 250, 30), menu[count]);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			// When the use key is pressed, the selected button will activate
+			buttons[selectedIndex] = true;
+		}
+		// Button conditions
+
+		if (buttons[0]) {
 			GameManager.RestartGame ();
-
-		if (GUI.Button(new Rect(Screen.width / 2 - 125, 200, 250, 30), "Main Menu"))
+		}
+		
+		if (buttons[1]) {
 			GameManager.GoToMainMenu ();
-
-		if (GUI.Button(new Rect(Screen.width / 2 - 125, 300, 250, 30), "Quit"))
+		}
+		// Cycling through buttons
+		if (buttons[2]) {
 			GameManager.QuitGame ();
+		}
+
+		if (Input.GetKeyDown(KeyCode.S)) {
+			if (selectedIndex == 0) {
+				selectedIndex = menu.Count - 1;
+			} else {
+				selectedIndex -= 1;
+			}
+			GUI.FocusControl(menu[selectedIndex]);
+		}
+
+		if (Input.GetKeyDown(KeyCode.W)) {
+			if (selectedIndex == menu.Count - 1) {
+				selectedIndex = 0;
+			} else {
+				selectedIndex += 1;
+			}
+			GUI.FocusControl(menu[selectedIndex]);
+		}
+
+
 	}
 }
